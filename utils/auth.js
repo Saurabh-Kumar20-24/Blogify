503e8503886bc2ed6fc3345d20b3870acb171fd7
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-06-19 16:12:14 +0530

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,14 @@
 const requireAuth = (req, res, next) =>{
+    console.log("requireAuth triggered. Session:", req.session);
     if(!req.session.userId){
+        console.log("Not authenticated. Redirecting to login."); 
        return res.redirect('/login')
     }
     next()
 }
 
 const checkAuth = (req, res, next) =>{
+    
     res.locals.isAuthenticated = req.session.userId ? true : false
     next()
 }
